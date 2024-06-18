7074a9c5a79b246ccb52e911ae010e6666e77f82
Andrei Harenkov <pevec1@yandex.ru>
2024-06-18 09:05:53 +0500

    cumul update commit 18-06-2024

--- src/App.jsx
+++ src/App.jsx
@@ -9,58 +9,110 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import TextField from "@mui/material/TextField";
+import Avatar from "@mui/material/Avatar";
+import { MarginOutlined } from "@mui/icons-material";
 function App() {
-  const [data, setData] = useState({ name: "", password: "" });
+  const [data, setData] = useState({ login: "", password: "" });
+  const [datasubmit, setDataSubmit] = useState({ login: "", password: "" });
+  const [token, setToken] = useState(null);
+  const [userResult, setUserResult] = useState(null);
+  const [news, setNews] = useState(null);
   const [flag, setFlag] = useState(false);
 
   const onChange = (event) => {
     const { target } = event;
     console.log(target);
     const { value } = target;
-    setData(value);
-    console.log("value:", value);
+    setData({ ...data, [target.name]: value });
   };
   const onSubmit = (event) => {
     event.preventDefault();
 
     const { target } = event;
     const formData = new FormData(target);
-
-    const data2 = Object.fromEntries(formData);
-    setData(data2);
-    loaddata();
-    console.log(data2, flag);
-    if (
-      data2.login === data.login &&
-      data2.password === data.password) {
-      setFlag(true);
-      console.log(data2, flag);
-    } else if (data2.message === "user not found") {
-      setFlag(false);
-      console.log(data2, flag);
-    }
+    setDataSubmit(Object.fromEntries(formData));
   };
   const loaddata = () => {
-    console.log(data);
-    fetch("https://backend-auth-0xh2.onrender.com/auth", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data, flag);
+    console.log(datasubmit);
+    setTimeout(() => {
+      fetch("https://backend-auth-0xh2.onrender.com/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(datasubmit),
       })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+        .then((response) => response.json())
+        .then((result) => {
+          if (result.message !== "user not found") {
+            setData(result.message);
+            setToken(result.token);
+            setFlag(false);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }, 1000);
   };
 
   useEffect(() => {
     loaddata();
-  }, [flag]);
+    // handleClickLogout();
+  }, [datasubmit]);
+
+  const funcGetToken = () => {
+    if (token) {
+      fetch("https://backend-auth-0xh2.onrender.com/private/me", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          setUserResult(result);
+          setFlag(true);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  };
+  console.log(userResult);
+  useEffect(() => {
+    funcGetToken();
+  }, [token]);
+
+  const funcGetNews = () => {
+    fetch("https://backend-auth-0xh2.onrender.com/private/news", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setNews(result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      })
+    }
+
+  useEffect(() => {
+    funcGetNews();
+  }, [token]);
+  const handleClickLogout = () => {
+    setDataSubmit({ login: "", password: "" });
+    setData({ login: "", password: "" });
+    setToken(null);
+    setUserResult(null);
+    setFlag(false);
+  };
 
   if (flag === true) {
     return (
@@ -77,10 +129,26 @@ function App() {
             }}
           >
             <Toolbar>
-              <Typography variant="h6" component="div">
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ MarginOutlined: "50px" }}
+              >
                 Neto Social
               </Typography>
-              <Button sx={{ bgcolor: "blue", color: "white" }} type="submit">
+              {userResult.avatar === "" ? null : (
+                <Typography variant="h6" component="div">
+                  {userResult.name}
+                </Typography>
+              )}
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt={userResult.name} src={userResult.avatar} />
+              </IconButton>
+
+              <Button
+                sx={{ bgcolor: "blue", color: "white" }}
+                onClick={handleClickLogout}
+              >
                 Logout
               </Button>
             </Toolbar>
@@ -89,17 +157,45 @@ function App() {
 
         <Box
           sx={{
+            margin: "auto",
             marginTop: "50px",
             padding: "20px",
-            width: "100%",
-            height: 300,
+            width: "80%",
+            height: "150vh",
             bgcolor: "lightgrey",
             fontSize: "30px",
           }}
         >
-          {flag}
           <h1>Neto Social</h1>
-          <p>Facebook and VK killer...</p>
+          <ul
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              fontSize: "15px",
+              width: "80%",
+              height: "100%",
+            }}
+          >
+            {news?.map((item) => (
+              <li
+                style={{
+                  margin: "10px",
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  width: "40%",
+                }}
+                key={item.id}
+              >
+                <h6>{item.title}</h6>
+                <p>
+                  <img src={item.image} alt={item.title} />
+                </p>
+                <p>{item.content}</p>
+              </li>
+            ))}
+          </ul>
         </Box>
       </>
     );
@@ -115,18 +211,19 @@ function App() {
               height: 100,
               bgcolor: "lightgrey",
               justifyContent: "center",
+              alignItems: "space-between",
             }}
           >
-            <Toolbar>
+            <Toolbar sx={{ justifyContent: "space-between" }}>
               <Typography variant="h6" component="div">
                 Neto Social
               </Typography>
               <TextField
                 required
                 id="outlined-required"
-                name="name"
+                name="login"
                 // defaultValue="User Name"
-                value={data.name}
+                value={data?.login || ""}
                 sx={{ bgcolor: "white" }}
                 onChange={onChange}
               />
@@ -135,7 +232,7 @@ function App() {
                 label="Password"
                 type="password"
                 name="password"
-                value={data.password}
+                value={data?.password || ""}
                 sx={{ bgcolor: "white" }}
                 autoComplete="current-password"
                 onChange={onChange}
@@ -157,7 +254,6 @@ function App() {
             fontSize: "30px",
           }}
         >
-          {flag}
           <h1>Neto Social</h1>
           <p>Facebook and VK killer...</p>
         </Box>
